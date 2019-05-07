de5b8dfb9b437edd4f5d9bf8ba9c53942e4bb2b2
colleowino <colleowino@gmail.com>
2019-05-07 18:22:25 +0300

    added linter

--- index.js
+++ index.js
@@ -1,77 +1,76 @@
-const scrapeIt = require("scrape-it");
+const scrapeIt = require('scrape-it');
 const download = require('image-downloader');
 const mkdirp = require('mkdirp');
 const path = require('path');
 
-// var url = "https://www.lapa.ninja"
-var url = 'http://www.landingfolio.com';
-var pgNum = process.argv[2];
+let url = 'http://www.landingfolio.com';
+const pageToLoad = process.argv[2];
+
+function logger(text) {
+  logger(text);
+}
+
+function downloadImage(imglink, filename) {
+  const folder = path.join(__dirname, 'downloads', pageToLoad.toString());
+
+  mkdirp(folder, (err) => {
+    if (err) logger(err);
+    else logger.log(`folder created: ${pageToLoad}`);
+  });
+
+  download.image({ url: imglink, dest: path.join(folder, filename) })
+    .then(({ newfilename }) => {
+      logger('completed ', path.basename(newfilename));
+    }).catch((err) => {
+      throw err;
+    });
+}
+
+function scrapePostPage(link) {
+  scrapeIt(link, {
+    imageLink: {
+      selector: '.featured-image a img',
+      attr: 'src',
+    },
+  }).then((res) => {
+    const linkname = link.split('/');
+    const filename = `${linkname[linkname.length - 2]}.jpg`;
+    downloadImage(res.imageLink, filename);
+  });
+}
 
 // Promise interface
-function scrapeListPage(pgNum){
-	// first page loads when page num excluded
-	if(pgNum > 1){
-			url += /page/+pgNum;
-	}
+function scrapeListPage(pageNumber) {
+// first page loads when page num excluded
+  if (pageNumber > 1) {
+    url = `${url}/page/${pageNumber}`;
+  }
 
-	scrapeIt(url, {
-			// fetch the screenshot pages
-			pagelinks: {
-				listItem: "div.col-md-4",
-				data: {
-					link: {
-						selector: 'a',
-						attr: "href"
-					}
-				}
-			}
-	}).then(page => {
-		var found = page.pagelinks;
-		for(var i = 0; i < found.length; i++){
-			screenPage = found[i].link;
+  scrapeIt(url, {
+  // fetch the screenshot pages
+    pagelinks: {
+      listItem: 'div.col-md-4',
+      data: {
+        link: {
+          selector: 'a',
+          attr: 'href',
+        },
+      },
+    },
+  }).then((page) => {
+    const found = page.pagelinks;
+    for (let i = 0; i < found.length; i += 1) {
+      const screenPage = found[i].link;
 
-			if(screenPage != undefined){
-				scrapePostPage(screenPage);
-			}
-		}
-
-	});
-
+      if (screenPage !== undefined) {
+        scrapePostPage(screenPage);
+      }
+    }
+  });
 }
 
-function scrapePostPage(link){
-
-	scrapeIt(link, {
-		imageLink : {
-			selector: ".featured-image a img",
-			attr: "src"
-		},
-	}).then( res => {
-		linkname = link.split("/");
-		filename = linkname[linkname.length - 2] + ".jpg"
-		downloadImage(res.imageLink, filename);
-	});
-}
-
-function downloadImage(imglink,filename){
-	var folder = path.join(__dirname,"downloads", pgNum.toString());
-
-	mkdirp(folder,function(err){
-		if(err) console.error(err)
-		// else console.log("folder created: "+pgNum);
-	});
-
-	download.image({url: imglink, dest: path.join(folder,filename)})
-		.then(({ filename, image }) => {
-			//console.log('completed ', path.basename(filename))
-		}).catch((err) => {
-			throw err
-		})
-
-}
-
-//var img_test = "https://www.lapa.ninja/assets/images/Frederique-Matti.jpg" 
-//downloadImage(2, img_test);
-// var linkpage = "http://www.landingfolio.com/gallery/headline/"
+// let img_test = "https://www.lapa.ninja/assets/images/Frederique-Matti.jpg"
+// downloadImage(2, img_test);
+// let linkpage = "http://www.landingfolio.com/gallery/headline/"
 // scrapePostPage(linkpage);
-scrapeListPage(pgNum);
+scrapeListPage(pageToLoad);
